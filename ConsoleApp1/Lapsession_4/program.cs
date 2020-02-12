0fec13e4b89f919ebb615deb4334d3be909dd20f
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2020-02-12 19:09:34 +0700

    lapssession_4

---
+++ ConsoleApp1/Lapsession_4/program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1.Lapsession_3
+{
+    public class program 
+    {
+        static void main(string[] args)
+        {
+            Thread t = new Thread(count);
+            // Thread t = new Thread(count);
+            t.Start();
+        }
+        public static void count()
+        {
+            for(int i = 600;i > 0; i--)
+            {
+                Console.WriteLine(i / 60 +":"+ i % 60);
+                Thread.Sleep(1000);
+            }
+        }
+        
+      
+    }
+}
+
+
